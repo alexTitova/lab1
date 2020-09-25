e62d6655260aacc158f8e55b938b8f905384e3d9
Александра Титова <alex.n.titova@gmail.com>
2020-09-25 12:46:53 +0300

    добавлен класс Path_unit

---
+++ Classes/path_unit.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1.Classes
+{
+    class Path_unit : IComparable<Path_unit> //  это единица списка пути
+    {
+        private char destination; // ключ
+    private int weight;
+    private bool flag; // пройдена ли данная вершина или нет
+
+
+    public Path_unit() { }
+
+    public Path_unit(char vertex, int weight_of_path)
+    {
+        destination = vertex;
+        weight = weight_of_path;
+        flag = false;
+    }
+
+
+    public char Destination
+    {
+        get { return destination; }
+        set { destination = value; }
+    }
+    public int Weight
+    {
+        get { return weight; }
+        set { weight = value; }
+    }
+    public bool Flag
+    {
+        get { return flag; }
+        set { flag = value; }
+    }
+
+
+
+
+    // Переопределение операторов сравнения
+    public static bool operator ==(Path_unit a1, Path_unit a2)
+    {
+        if (a1.destination == a2.destination)
+            return true;
+        else
+            return false;
+    }
+
+    public static bool operator !=(Path_unit a1, Path_unit a2)
+    {
+        if (a1.destination != a2.destination)
+            return true;
+        else
+            return false;
+    }
+
+    public static bool operator <(Path_unit a1, Path_unit a2)
+    {
+        if (a1.destination < a2.destination)
+            return true;
+        else
+            return false;
+    }
+
+    public static bool operator >(Path_unit a1, Path_unit a2)
+    {
+        if (a1.destination != a2.destination)
+            return true;
+        else
+            return false;
+    }
+
+    public static bool operator <=(Path_unit a1, Path_unit a2)
+    {
+        if (a1.destination <= a2.destination)
+            return true;
+        else
+            return false;
+    }
+
+    public static bool operator >=(Path_unit a1, Path_unit a2)
+    {
+        if (a1.destination >= a2.destination)
+            return true;
+        else
+            return false;
+    }
+
+    public int CompareTo(Path_unit obj) // реализация для интерфейса IComparable<> для Sort
+    {
+        return this.destination.CompareTo(obj.destination);
+    }
+
+}
+
+
+}
